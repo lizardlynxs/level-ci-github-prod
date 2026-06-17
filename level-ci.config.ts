4a7b23b37d75dbaf9aabc8d93d71ddcdc8d9b09f
Yelyzaveta Gorbunova <yelyzaveta.gorbunova@levelaccess.com>
2026-06-17 09:41:30 +0300

    init

---
+++ level-ci.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "@level-ci/cli";
+
+export default {
+  organization: "sprintdemo-13-05-rylzz",
+  project: "level-ci-dev",
+  token: 'p4vtwynsghcwbniag5y7ueprpwrrkwdrwxc9xtdj',
+  reportPaths: ["./level-ci-reports"],
+  server: "https://api.dev.userway.dev",
+} satisfies Config;
